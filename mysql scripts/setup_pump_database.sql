89bf2ebc862728e35c4414bbe1d1bd2b8ceedabe
Michael Khuri <michaelkhuri@gmail.com>
2025-03-27 15:28:16 -0700

    setup calibration column info

--- mysql scripts/setup_pump_database.sql
+++ mysql scripts/setup_pump_database.sql
@@ -51,5 +51,6 @@ CREATE TABLE `pumps` (
      `Port` VARCHAR(255) NULL,
      `Pulsation_Dampener` DECIMAL(10,4) NULL,
      `Calibration_Column` DECIMAL(10,4) NULL,
+     `Calibration_Column_Info` VARCHAR(255) NULL,
      `Pressure_Gauge` DECIMAL(10,4) NULL
 );
