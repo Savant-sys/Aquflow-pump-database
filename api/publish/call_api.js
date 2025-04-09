46ad744efd3535dc399173ba7f005fa974d02e55
Michael Khuri <michaelkhuri@gmail.com>
2025-04-09 14:06:23 -0700

    update api server

--- api/publish/call_api.js
+++ api/publish/call_api.js
@@ -629,31 +629,31 @@ function formatNumberWithCommas(number) {
 function generateOptionalAccessoriesSection(data) {
     const accessories = [
         data.spare_parts_kit === 'Yes' ? 
-            `<p>Spare Parts Kit ${data.spare_parts_kit_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.spare_parts_kit_price || 0)}`}</p>` 
+            `<p>Spare Parts Kit: ${data.spare_parts_kit_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.spare_parts_kit_price || 0)}`}</p>` 
             : '',
         data.degassing === 'Yes' ? 
-            `<p>Degassing Valve ${data.degassing_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.degassing_price || 0)}`}</p>` 
+            `<p>Degassing Valve: ${data.degassing_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.degassing_price || 0)}`}</p>` 
             : '',
         data.back_pressure_valve === 'Yes' ? 
-            `<p>Back Pressure Valve ${data.back_pressure_valve_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.back_pressure_valve_price || 0)}`}</p>` 
+            `<p>Back Pressure Valve: ${data.back_pressure_valve_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.back_pressure_valve_price || 0)}`}</p>` 
             : '',
         data.pressure_relief_valve === 'Yes' ? 
-            `<p>Pressure Relief Valve ${data.pressure_relief_valve_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.pressure_relief_valve_price || 0)}`}</p>` 
+            `<p>Pressure Relief Valve: ${data.pressure_relief_valve_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.pressure_relief_valve_price || 0)}`}</p>` 
             : '',
         data.pulsation_dampener === 'Yes' ? 
-            `<p>Pulsation Dampener ${data.pulsation_dampener_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.pulsation_dampener_price || 0)}`}</p>`
+            `<p>Pulsation Dampener: ${data.pulsation_dampener_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.pulsation_dampener_price || 0)}`}</p>`
             : '',
         data.calibration_column === 'Yes' ? 
-            `<p>Calibration Column ${data.calibration_column_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.calibration_column_price || 0)}`}</p>` 
+            `<p>Calibration Column: ${data.calibration_column_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.calibration_column_price || 0)}`}</p>` 
             : '',
         data.pressure_gauge === 'Yes' ? 
-            `<p>Pressure Gauge ${data.pressure_gauge_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.pressure_gauge_price || 0)}`}</p>` 
+            `<p>Pressure Gauge: ${data.pressure_gauge_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.pressure_gauge_price || 0)}`}</p>` 
             : '',
         data.ecca === 'Yes' ? 
-            `<p>ECCA ${data.ecca_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.ecca_price || 0)}`}</p>` 
+            `<p>ECCA: ${data.ecca_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.ecca_price || 0)}`}</p>` 
             : '',
         data.vfd === 'Yes' ? 
-            `<p>VFD ${data.vfd_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.vfd_price || 0)}`}</p>` 
+            `<p>VFD: ${data.vfd_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.vfd_price || 0)}`}</p>` 
             : '',
         data.leak_detection && data.leak_detection !== 'No' ? 
             `<p>Leak Detection System: ${data.leak_detection} ${data.leak_detection === 'Conductive' ? (data.relay_option === 'Yes' ? 'with Relay' : 'without Relay') : ''} ${data.leak_detection_price === "C/F" ? "C/F" : `$${formatNumberWithCommas(data.leak_detection_price || 0)}`}</p>` 
